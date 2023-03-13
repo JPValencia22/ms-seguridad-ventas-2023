e3ee1f7b9a30bdeb712c769dd497fe2dbee88fdc
Juan Pablo Valencia <pibe742337@gmail.com>
2023-03-13 11:39:12 -0500

    Creación permisos de un rol en los menus y ajustes en la strategy y seguridad-usuario.service

--- src/config/seguridad.config.ts
+++ src/config/seguridad.config.ts
@@ -1,3 +1,9 @@
 export namespace ConfiguracionSeguridad{
   export const claveJWT = "Admin@2023*";
+  export const menuUsuarioId = "640cecc7a49bf262bc70a808";
+  export const listarAccion = "listar";
+  export const guardarAccion = "guardar";
+  export const editarAccion = "editar";
+  export const eliminarAccion = "eliminar";
+  export const descargarAccion = "descargar";
 }
